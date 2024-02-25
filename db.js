31a34713f99d5d523c31f69980e6b6e7ab8cbfc6
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-02-25 20:31:12 +0530

    Initial Commit

--- db.js
+++ db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 //Creating a database by the name of PRODUCTS.
 
-mongoose.connect("mongodb+srv://shuv009:GKw2I3b8xDelXQBO@cluster0.qjktjnz.mongodb.net/PRODUCTS?retryWrites=true&w=majority&appName=Cluster0").then(() =>{
+const url = "mongodb+srv://shuv009:GKw2I3b8xDelXQBO@cluster0.qjktjnz.mongodb.net/PRODUCTS?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(url).then(() =>{
     console.log("Connection Sucessful.");
 }).catch((err)=>{
     console.log(err);
